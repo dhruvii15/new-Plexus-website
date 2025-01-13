e9c865acc466da534ae78477497f4362612765c6
plexustechnology <dhruvi.plexustechnology@gmail.com>
2025-01-13 11:25:50 +0530

    changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,61 +3,34 @@ import Loading from '../Component/Loading';
 import { toast } from 'react-toastify';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPersonCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 const Header = lazy(() => import('../Component/Header'));
 const About = lazy(() => import('../Component/Home/About'));
 
 const Home = () => {
-    const [portfolioData, setPortfolioData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState(false);
-    const [worker, setWorker] = useState(null);
 
-    console.log(portfolioData, data);
+    const getData = () => {
+        axios.get('https://plexus-technology.in/api/admin/read')
+            .then((res) => {
+                setData(res.data.data[0].hiringStatus);
+            })
+            .catch((err) => {
+                console.error(err);
+                toast.error("Failed to fetch data.");
+            });
+    };
 
 
     useEffect(() => {
-        // Initialize Web Worker
-        const newWorker = new Worker(new URL('../Worker/worker.js', import.meta.url));
-        setWorker(newWorker);
-
-        // Set up message handlers
-        newWorker.onmessage = (event) => {
-            const { type, data: responseData, error } = event.data;
-
-            switch (type) {
-                case 'PORTFOLIO_DATA':
-                    setPortfolioData(responseData);
-                    setLoading(false);
-                    break;
-                case 'ADMIN_DATA':
-                    setData(responseData);
-                    break;
-                case 'ERROR':
-                    console.error('Worker error:', error);
-                    toast.error("Failed to fetch data.");
-                    setLoading(false);
-                    break;
-                default:
-                    break;
-            }
-        };
-
-        // Start fetching data in background
-        newWorker.postMessage({ type: 'FETCH_PORTFOLIO' });
-        newWorker.postMessage({ type: 'FETCH_ADMIN' });
-
-        // Cleanup worker on component unmount
-        return () => {
-            if (newWorker) {
-                newWorker.terminate();
-            }
-        };
+        getData();
     }, []);
 
     return (
         <>
-            {loading ? (
+            {!loading ? (
                 <Loading />
             ) : (
                 <Suspense fallback={<Loading />}>
