032c17ba33cb83658c60d2e025d0a0828659d9d8
plexustechnology <dhruvi.plexustechnology@gmail.com>
2025-01-13 17:37:12 +0530

    footer changes

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -31,7 +31,7 @@ const Footer = () => {
 
 
     return (
-        <div style={{ backgroundColor: "#080808", color: "#848484" }} className='overflow-hidden mx-4 rounded-5'>
+        <div style={{ backgroundColor: "#080808", color: "#848484" }} className='overflow-hidden mx-4 rounded-5 mb-3'>
             <Container>
 
                 <Row className='d-flex align-items-center justify-content-center py-5 px-2'>
@@ -51,16 +51,15 @@ const Footer = () => {
                     <Col xs={12} lg={7}>
                         <div className='text-center pt-4 footer-width'>
                             <img src={logo} alt='plexus-technology' className='img-fluid' width={220} />
-                            <div className='d-flex align-items-center justify-content-center gap-3 py-2'>
-
-                                <div onClick={handleLinkedInClick}>
-                                    <FontAwesomeIcon icon={faLinkedin} className="fs-4" />
+                            <div className='d-flex align-items-center justify-content-center gap-3 py-2 text-white'>
+                                <div className="common-footer hover-box box1" onClick={handleLinkedInClick}>
+                                    <FontAwesomeIcon icon={faLinkedin} className="hover-box2 fs-4" />
                                 </div>
-                                <div onClick={handleWhatsappClick}>
-                                    <FontAwesomeIcon icon={faWhatsapp} className="fs-4" />
+                                <div className="common-footer hover-box box2" onClick={handleWhatsappClick}>
+                                    <FontAwesomeIcon icon={faWhatsapp} className="hover-box2 fs-4" />
                                 </div>
-                                <div onClick={handleInstaClick}>
-                                    <FontAwesomeIcon icon={faInstagram} className="fs-4" />
+                                <div className="common-footer hover-box box3" onClick={handleInstaClick}>
+                                    <FontAwesomeIcon icon={faInstagram} className="hover-box2 fs-4" />
                                 </div>
                             </div>
                             <p style={{ fontSize: "14px" }} className='py-2'>All rights Reseverd . © Plexus Technology </p>
